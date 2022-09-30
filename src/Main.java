0ae64bae1b3241e2f9bf3e3eff1a9101e8fb2017
Imdad Rind <1103852@stud.uot.edu.pk>
2022-09-30 07:44:37 -0700

    inserionSort is compled

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        //System.out.println("Hello world!");
+        inserionSort mysort = new inserionSort();
+        int[] myArray = {6,5,9,2,0,1,8,3,4,7};
+        mysort.arraySort(myArray);
+    }
+}
